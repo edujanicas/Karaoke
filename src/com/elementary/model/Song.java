9873c36fce4494aed4d2ff1e58618a53243946ac
edujanicas <edujanicas@icloud.com>
2017-01-29 14:23:32 +0000

    Class Song built

--- src/com/elementary/model/Song.java
+++ src/com/elementary/model/Song.java
@@ -4,4 +4,22 @@ package com.elementary.model;
  * Created by ejanicas on 29/01/17.
  */
 public class Song {
+    private String mArtist;
+    private String mTitle;
+    private String mVideoUrl;
+
+    public Song(String artist, String title, String videoUrl) {
+        mArtist = artist;
+        mTitle = title;
+        mVideoUrl = videoUrl;
+    }
+
+    public String getTitle() { return mTitle; }
+    public String getArtist() { return mArtist; }
+    public String getVideoUrl() { return mVideoUrl; }
+
+    @Override
+    public String toString() {
+        return String.format("Song: %s by %s", mTitle, mArtist);
+    }
 }
