8010eab077665e901247c4a38d9329a48e99177c
edujanicas <edujanicas@icloud.com>
2017-01-29 14:26:38 +0000

    Class SongBook

---
+++ src/com/elementary/model/SongBook.java
@@ -0,0 +1,22 @@
+package com.elementary.model;
+
+import java.util.List;
+import java.util.ArrayList;
+/**
+ * Created by ejanicas on 29/01/17.
+ */
+public class SongBook {
+    private List<Song> mSongs;
+
+    public SongBook() {
+        mSongs = new ArrayList<Song>();
+    }
+
+    public void addSong(Song song) {
+        mSongs.add(song);
+    }
+
+    public int getSongCount() {
+        mSongs.size();
+    }
+}
